ad0976662db6512b4a88171fea0149ac4a7aff34
23rdPro <mailolumide@gmail.com>
2023-03-17 00:08:03 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    version='1.0',
+    description='Emergency Response Pub/Sub App',
+    author='Olumide Bakare',
+    author_email='mailolumide@gmail.com',
+    name='eRS',
+    packages=find_packages(),
+    # packages=['eEvent'],
+    long_description='''
+    Data analysis/mining on user engagement with the eResponse 
+    system to reduce resource mismanagement...
+    ''',
+    maintainer='Olumide Bakare',
+    maintainer_email='mailolumide@gmail.com',
+    url='https://github.com/23rdPro/eResponse',
+)
