21372c074100b2d3dae34d1cdcf9f5999ae64f53
23rdPro <mailolumide@gmail.com>
2023-04-12 08:40:39 +0100

    refactored app name for response app

--- eResponse/response/apps.py
+++ eResponse/response/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ResponseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'response'
+    name = 'eResponse.response'
