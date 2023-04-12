e63581ec634a468f9f513dbc01ea85d69ebdca6c
23rdPro <mailolumide@gmail.com>
2023-04-12 20:34:36 +0100

    refactored celery broker and tasks autodiscovery

--- eResponse/celery.py
+++ eResponse/celery.py
@@ -1,17 +1,18 @@
 import os
 from celery import Celery
+from django.conf import settings
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eResponse.settings')
 
-app = Celery('eResponse')
+app = Celery('eResponse', broker=settings.CELERY_BROKER_URL)
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 
-@app.task(bind=True, ignore_result=True)
+@app.task(bind=True)  # ignore_result=True
 def debug_task(self):
     print(f'Request: {self.request!r}')
 
