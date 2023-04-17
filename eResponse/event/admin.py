3eb90eddbf807d2b664648561ae0f875db355c21
23rdPro <mailolumide@gmail.com>
2023-04-17 12:21:45 +0100

    added admin for AdminLastEventToken

--- eResponse/event/admin.py
+++ eResponse/event/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 
-from eResponse.event.models import ThreadEvent
+from eResponse.event.models import ThreadEvent, Role, LastEventToken
 
 
 @admin.register(ThreadEvent)
 class AdminThreadEvent(admin.ModelAdmin):
     list_display = 'id', 'timestamp'
+
+
+@admin.register(Role)
+class AdminRole(admin.ModelAdmin):
+    list_display = 'id', 'timestamp'
+
+
+@admin.register(LastEventToken)
+class AdminLastEventToken(admin.ModelAdmin):
+    list_display = 'token',
