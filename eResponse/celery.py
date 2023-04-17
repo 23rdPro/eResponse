32458a1252ac5baafaf702d3d6e96d1ad7255f9c
23rdPro <mailolumide@gmail.com>
2023-04-17 12:20:04 +0100

    set celery ignore_result to true

--- eResponse/celery.py
+++ eResponse/celery.py
@@ -9,10 +9,10 @@ app = Celery('eResponse', broker=settings.CELERY_BROKER_URL)
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.autodiscover_tasks()  # lambda: settings.INSTALLED_APPS
 
 
-@app.task(bind=True)  # ignore_result=True
+@app.task(bind=True, ignore_result=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
 
