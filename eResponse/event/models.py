785319b184f47c924df06615f9c899db9d116a11
23rdPro <mailolumide@gmail.com>
2023-04-17 21:07:53 +0100

    added response_time to fields on Role

--- eResponse/event/models.py
+++ eResponse/event/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.utils import timezone
 
 
+class LastEventToken(models.Model):
+    token = models.CharField(primary_key=True, unique=True, editable=False)
+    objects = models.Manager()
+
+
 class ThreadEvent(models.Model):
     """
     Each event is a message. Messages in any ThreadEvent can either
@@ -14,23 +19,24 @@ class ThreadEvent(models.Model):
     """
     timestamp = models.DateTimeField(default=timezone.now)
     # timestamp when new thread event is recorded the first time
-    id = models.CharField(max_length=128, primary_key=True, editable=False, unique=True)
+    id = models.CharField(
+        max_length=128, primary_key=True, editable=False, unique=True
+    )
     roles = models.ManyToManyField('Role')  # cannot be blank
-    objects = models.Manager()
-
-
-class LastEventToken(models.Model):
-    token = models.CharField(primary_key=True, unique=True, editable=False)
     objects = models.Manager()
 
 
 class Role(models.Model):
     # role = models.CharField(max_length=1, choices=ROLES)
-    id = models.CharField(max_length=128, primary_key=True, editable=False, unique=True)
+    id = models.CharField(
+        max_length=128, primary_key=True, editable=False, unique=True
+    )
     timestamp = models.DateTimeField(default=timezone.now)
     # timestamps when multiple responses are being added to ThreadEvent.roles
 
-    response_time = models.DecimalField(decimal_places=3, max_digits=12)
-    # approximate time to add to ThreadEvent[id].roles
+    response_time = models.DecimalField(
+        decimal_places=3, max_digits=12, blank=True, null=True
+    )
+    # approximate time for each role from ThreadEvent[id]
 
     objects = models.Manager()
