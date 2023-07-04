013a976156378550defd7ea17228daddc4e85424
23rdPro <mailolumide@gmail.com>
2023-07-04 22:38:00 +0100

    optimized routine db access

--- eResponse/event/tasks.py
+++ eResponse/event/tasks.py
@@ -24,6 +24,54 @@ def call_service() -> None:
     service.users().watch(userId='me', body=request).execute()
 
 
+@celery.shared_task()
+def complete_pubsub_task():
+    if LastEventToken.objects.exists():
+        token = LastEventToken.objects.last()
+        latest_threads = service.users().threads().list(
+            userId='me', includeSpamTrash=False, pageToken=token
+        ).execute()
+    else:
+        latest_threads = service.users().threads().list(
+            userId='me', includeSpamTrash=False
+        ).execute()
+
+    # cycle to the last page of threads
+    token = None
+    while 'nextPageToken' in latest_threads:
+        token = latest_threads['nextPageToken']
+
+        latest_threads = service.users().threads().list(
+            userId='me', includeSpamTrash=False, pageToken=token
+        ).execute()
+    else:
+        if token is not None:
+            LastEventToken(token=token).save()
+
+    # assert 'threads' in latest_threads
+    if 'threads' in latest_threads:
+        # atomic transaction favors credible db writes
+        with transaction.atomic():
+            # create threads from the list of threads in bulk
+            events = [
+                ThreadEvent.objects.create(id=thread['id'])
+                if not ThreadEvent.objects.filter(id=thread['id']).exists()
+                else ThreadEvent.objects.get(id=thread['id'])
+                for thread in latest_threads['threads']
+            ]
+            # create roles for each thread in the list of messages in bulk
+            for i, event in enumerate(events):
+                thd = service.users().threads().get(
+                    userId='me', id=event.id, format='minimal'
+                ).execute()
+
+                event.roles.add(*Role.objects.bulk_create([
+                    Role(id=msg['id']) for msg in
+                    thd['messages'][event.roles.all().count():]
+                ]))
+    return
+
+
 def _run_process(threads: dict) -> None:
     pageToken = None
 
