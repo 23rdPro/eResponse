056e6d200027828e15bbbc2c350c78cb79a68784
23rdPro <mailolumide@gmail.com>
2023-07-05 22:34:18 +0100

    mounted api on WSGI

--- eResponse/wsgi.py
+++ eResponse/wsgi.py
@@ -1,16 +1,18 @@
-"""
-WSGI config for eResponse project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
+from fastapi import FastAPI
+
+from eResponse.user.FastAPI.urls import router
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eResponse.settings')
 
 application = get_wsgi_application()
+
+app = FastAPI(
+    title="Emergency Response App",
+    description="",
+    version="",
+)
+
+app.include_router(router=router, prefix='/v1')
