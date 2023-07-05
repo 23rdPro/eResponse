2f2553de39ae9962696f4c4708ddf4e6f28b6a09
23rdPro <mailolumide@gmail.com>
2023-07-05 11:30:31 +0100

    created model for Emergency

--- eResponse/response/models.py
+++ eResponse/response/models.py
@@ -1,34 +1,82 @@
+"""
+Emergency Model provides the identification, and the remedying activities at instantiation
+There will be two major groups of Emergency: natural and synthetic, and must be associated
+with at least one management level user.
+"""
+
 import uuid
+import logging
 
 from django.db import models
 from django.contrib.auth import get_user_model
-from django.utils import timezone
+from django.contrib.auth.models import Group
+from django.utils.translation import gettext_lazy as _
+
+from eResponse.mixins import TimeMixin
 
 
-class EmergencyEvent(models.Model):
-    estatus = (
-        (NOT_HANDLED    := '1', 'NOT_HANDLED'),
-        (BEING_HANDLED  := '2', 'BEING_HANDLED'),
-        (HANDLED        := '3', 'HANDLED'),
+class Emergency(TimeMixin):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='type')
+
+    # all users regardless of groups however a manager must instantiate
+    # this model hence, cannot be blank
+    respondents = models.ManyToManyField(get_user_model(), related_name='experts')
+
+    # each emergency may have multiple briefs
+    briefs = models.ManyToManyField("Brief", related_name='briefs')
+
+    class Severity(models.IntegerChoices):
+        BAD = 1, "Bad"
+        TERRIBLE = 2, "Terrible"
+        CATACLYSMIC = 3, "Cataclysmic"
+
+    severity = models.IntegerField(
+        choices=Severity.choices,
+        default=Severity.BAD,
+        verbose_name="Severity"
     )
 
+    class EmergencyQuerySet(models.QuerySet):
+        def _check(self):
+            try:
+                assert self.filter().exists()
+                assert 'experts' in self.filter(respondents__groups__name='experts')
+                assert 'leads' in self.filter(respondents__groups__name='leads')
+                assert 'managers' in self.filter(respondents__groups__name='managers')
+            except Exception as error:
+                logging.error(error)
+
+        def experts(self):
+            self._check()
+            return self.filter(respondents__groups__name__contains='experts')
+
+        def leads(self):
+            self._check()
+            return self.filter(respondents__groups__name__contains='leads')
+
+        def managers(self):
+            self._check()
+            return self.filter(respondents__groups__name__contains='managers')
+
+    objects = EmergencyQuerySet.as_manager()
+
+
+class Brief(TimeMixin):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True)
-    managers = models.ManyToManyField(get_user_model())
-    actors = models.ManyToManyField(get_user_model())
-    status = models.CharField(max_length=1, choices=estatus)
-    reports = models.ManyToManyField('Report')
-    timestamp = models.DateTimeField(default=timezone.now)
+    title = models.CharField(_("Title Description"), max_length=255)
+    text = models.TextField(_("Text"), max_length=500, blank=False, null=False)
+    pictures = models.ManyToManyField("Picture", blank=True, related_name='pictures')
+    videos = models.ManyToManyField("Video", blank=True, related_name='videos')
+    objects = models.Manager()
 
 
-class Report(models.Model):
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    event = models.OneToOneField(EmergencyEvent, on_delete=models.CASCADE)
-    title = models.CharField(max_length=500)
-    description = models.TextField(max_length=1000)
-    attachments = models.FileField(upload_to=f'../media/{user.username}/')
-    timestamp = models.DateTimeField(default=timezone.now)
+class Picture(TimeMixin):
+    picture = models.FileField(upload_to='jpegs/%Y/%m/%d/')
+    objects = models.Manager()
+    # attachments = models.FileField(upload_to=f'../media/{user.username}/') todo
 
-    def verify_attachments(self):
-        if self.attachments > 2048:
-            raise
-        return self.attachments < 2048  # todo
+
+class Video(TimeMixin):
+    video = models.FileField(upload_to='tapes/%Y/%m/%d/')
+    objects = models.Manager()
