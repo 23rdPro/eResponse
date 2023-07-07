22c16409fb035d49460f65856e63d4d2c1fb57a6
23rdPro <mailolumide@gmail.com>
2023-07-07 16:57:21 +0100

    added dependencies to sapi router

--- eResponse/urls.py
+++ eResponse/urls.py
@@ -1,7 +1,20 @@
+import typing
 from django.contrib import admin
 from django.urls import path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
+
+from fastapi import APIRouter, Depends
+
+from . import oauth2_scheme
+
+router = APIRouter()
+
+router.get(
+    '/users',
+    dependencies=[Depends(oauth2_scheme)],
+    # responses={404: {"description": "Not found"}},
+)
 
 
 urlpatterns = [
