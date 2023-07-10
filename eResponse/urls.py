1c87de330b3f0cfa417aabb3e45b409f95affc35
23rdPro <mailolumide@gmail.com>
2023-07-10 17:33:05 +0100

    added post route for users endpoint

--- eResponse/urls.py
+++ eResponse/urls.py
@@ -1,27 +1,28 @@
 import typing
-from django.contrib import admin
+from django.contrib import admin, auth
 from django.urls import path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
-
 from fastapi import APIRouter, Depends
-
 from . import oauth2_scheme
+from eResponse.user import UserType
+from eResponse.user.FastAPI import views, schemas
+from eResponse.user import models
 
 router = APIRouter()
 
 router.get(
     '/users',
-    dependencies=[Depends(oauth2_scheme)],
+    # dependencies=[Depends(oauth2_scheme)],
     # responses={404: {"description": "Not found"}},
-)
+    # response_model=schemas.UsersSchema,
+    tags=["users", "experts", ],
+    summary="Retrieve all users",
+    status_code=200,
+)(views.read_users)
 
-
-urlpatterns = [
-    # path('', admin.site.urls),
-    path('admin/', admin.site.urls),
-]
-
-
-if settings.DEBUG:
-    urlpatterns += staticfiles_urlpatterns()
+router.post(
+    '/users',
+    status_code=200,
+    summary="Post to user"
+)(views.create_user)
