9e511f400d6ab26a4a2a24371675a6dda049cd8c
23rdPro <mailolumide@gmail.com>
2023-07-10 17:36:17 +0100

    added settings for user signals

--- eResponse/user/apps.py
+++ eResponse/user/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class UserConfig(AppConfig):
     name = 'eResponse.user'
+
+    def ready(self):
+        from . import signals
+        import eResponse.user.signals
