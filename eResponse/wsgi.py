33fe578fca9e4a32c8a7b965ddfe1d439060b0dc
23rdPro <mailolumide@gmail.com>
2023-07-10 23:26:01 +0100

    added cors middleware

--- eResponse/wsgi.py
+++ eResponse/wsgi.py
@@ -1,18 +1,35 @@
 import os
+import django
 
 from django.core.wsgi import get_wsgi_application
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from eResponse.user.FastAPI.urls import router
+django.setup()
+
+from .urls import router  # noqa: E402
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eResponse.settings')
+
+# os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"  todo: safe?
 
 application = get_wsgi_application()
 
 app = FastAPI(
     title="Emergency Response App",
     description="",
-    version="",
+    version="1.0",
 )
 
-app.include_router(router=router, prefix='/v1')
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+app.include_router(router=router, prefix='/api/v1')
+
