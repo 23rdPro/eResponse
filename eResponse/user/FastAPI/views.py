d0e12773f24694861b72d55e03a5717979c2d385
23rdPro <mailolumide@gmail.com>
2023-07-11 20:41:48 +0100

    implemented views procedures for interface

--- eResponse/user/FastAPI/views.py
+++ eResponse/user/FastAPI/views.py
@@ -1,10 +1,13 @@
 import json
+import logging
+
 import eResponse
 import typing
 import httpx
+import uuid
 import asyncio
 from asgiref.sync import sync_to_async
-from typing import List, Dict, Coroutine
+from typing import List, Dict, Coroutine, Union, Tuple, Annotated
 
 from django.conf import settings
 from django.contrib.auth.models import Group
@@ -14,11 +17,12 @@ from django.views.decorators.csrf import csrf_protect
 from django.http import JsonResponse, HttpRequest
 from django.db import transaction
 
-from fastapi import Request, HTTPException, Depends, FastAPI
+from fastapi import Request, HTTPException, Depends, FastAPI, Response, File
 # from eResponse.urls import router
 from eResponse.user import models
 
 from eResponse.user.FastAPI.schemas import UserSchema, GroupSchema
+from eResponse import oauth2_scheme
 
 # User = get_user_model()
 
@@ -26,77 +30,30 @@ URL = ""
 
 
 async def read_users() -> Dict[str, List]:
-    queryset: List = await sync_to_async(list)(
+    queryset: Union[Tuple, Coroutine] = await sync_to_async(tuple)(
         models.User.users.get_users())
 
-    return {"users": UserSchema.from_django(queryset, many=True)}
+    return await sync_to_async(lambda: {
+        "users": UserSchema.from_django(queryset, many=True)})()
 
 
-async def create_user(email, password):
-    user = await models.User.objects.acreate(email=email, password=password)
-    return UserSchema.from_orm(user)
+async def create_user(schema_type: UserSchema) -> UserSchema:
+    data: UserSchema = await sync_to_async(lambda: schema_type)()
+    await sync_to_async(lambda: models.User.from_api(data).save())()
+    return data
 
 
-# # @router.get("")
-# def read_managers():
-#     queryset = User.users.get_managers()
-#
-#
-# # @router.get("")
-# def read_experts():
-#     queryset = User.users.get_experts()
-#
-#
-# # @router.get("")
-# def read_leads():
-#     queryset = User.users.get_leads()
-#
-#
-# @csrf_protect
-# @require_safe
-# @require_http_methods(["POST", "GET", "PUT"])
-# def users_view(request: Request):
-#     if request.method == "POST":
-#         pass
-#
-#
-# @csrf_protect
-# @require_http_methods(["POST", "GET"])
-# def create_user(request: HttpRequest) -> JsonResponse:
-#     """
-#     create user object with email and password
-#     flow: the user must activate account and complete other
-#     registration steps before full authorization is granted.
-#     :param request:
-#     :return: [JsonResponse]
-#     """
-#     try:
-#         if request.method == "POST":
-#             data: dict = json.loads(request.body)
-#             user: User = User.objects.create_user(**data)
-#
-#             schema = eResponse.user.FastAPI.schemas.UserSchema.from_django(user)
-#             return JsonResponse({'user': schema.dict()})
-#
-#     except Exception as error:
-#         raise HTTPException(status_code=400)
-#         # return JsonResponse({"detail": str(error)}, status=status)
-#
-#
-#
-# def get_respondent(request):
-#     pass
-#
-#
-# def update_respondent(request):
-#     pass
-#
-#
-# def add_certification(request):
-#     pass
-#
-#
-# def update_certification(request):
-#     pass
-#
-#
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    users = await sync_to_async(list)(models.User.users.get_experts().filter(id=token))
+    assert len(users) == 1
+    user = users.pop()
+    try:
+        user = models.User.objects.aget(user.id)
+    except Exception as Error:
+        logging.error(Error)
+    finally:
+        return await sync_to_async(lambda: {"user": UserSchema.from_orm(user)})()
+
+
+async def read_users_me(current_user: Annotated[UserSchema, Depends(get_current_user)]):
+    return current_user
