69fe5632c6d9123ce3db7dc3281a5640b9f2e9ae
23rdPro <mailolumide@gmail.com>
2023-07-11 20:36:09 +0100

    added config for project logs

--- eResponse/__init__.py
+++ eResponse/__init__.py
@@ -1,6 +1,8 @@
+import logging
 from .celery import app as celery_app
 from fastapi.security import OAuth2PasswordBearer
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+logging.basicConfig(filename='appinfo.log', level=logging.DEBUG)
 
 __all__ = ('celery_app', 'oauth2_scheme')
