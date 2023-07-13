a220d11c62249e113389aeda4520039ed2168401
23rdPro <mailolumide@gmail.com>
2023-07-13 23:46:03 +0100

    refactored token url to meet spec

--- eResponse/__init__.py
+++ eResponse/__init__.py
@@ -2,7 +2,7 @@ import logging
 from .celery import app as celery_app
 from fastapi.security import OAuth2PasswordBearer
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/token")
 logging.basicConfig(filename='appinfo.log', level=logging.DEBUG)
 
 __all__ = ('celery_app', 'oauth2_scheme')
