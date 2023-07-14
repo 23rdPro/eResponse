e05773cd97d91181f9df68400adc275ca88f9554
23rdPro <mailolumide@gmail.com>
2023-07-14 19:53:44 +0100

    updated __all__ with app-level var

--- eResponse/__init__.py
+++ eResponse/__init__.py
@@ -1,8 +1,17 @@
 import logging
+import os
 from .celery import app as celery_app
 from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/token")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 logging.basicConfig(filename='appinfo.log', level=logging.DEBUG)
 
-__all__ = ('celery_app', 'oauth2_scheme')
+
+API_SECRET_KEY = os.getenv('API_SECRET_KEY')
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+
+__all__ = ('celery_app', 'oauth2_scheme', 'pwd_context', 'ALGORITHM', 'API_SECRET_KEY', 'ACCESS_TOKEN_EXPIRE_MINUTES')
