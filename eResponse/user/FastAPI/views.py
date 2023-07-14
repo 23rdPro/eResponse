01067fe9f4f66dd2d80649b1e06edc00f76460f8
23rdPro <mailolumide@gmail.com>
2023-07-14 11:25:34 +0100

    fixed synchronuous issue with user auth

--- eResponse/user/FastAPI/views.py
+++ eResponse/user/FastAPI/views.py
@@ -9,32 +9,33 @@ import asyncio
 from asgiref.sync import sync_to_async
 from typing import List, Dict, Coroutine, Union, Tuple, Annotated
 
+from djantic import ModelSchema
+
 from django.conf import settings
 from django.contrib.auth.models import Group
 # from django.contrib.auth import get_user_model
 from django.views.decorators.http import require_http_methods, require_safe
 from django.views.decorators.csrf import csrf_protect
 from django.http import JsonResponse, HttpRequest
-from django.db import transaction
+from django.db import transaction, models
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth import authenticate, login as user_login
 
 from fastapi import Request, HTTPException, Depends, FastAPI, Response, File, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm, OAuth2PasswordRequestFormStrict
 # from eResponse.urls import router
-from eResponse.user import models
+from eResponse.user import models as user_models
 
 from eResponse.user.FastAPI.schemas import UserSchema, GroupSchema
 from eResponse import oauth2_scheme
 
-# User = get_user_model()
 
 URL = ""
 
 
 async def read_users() -> Dict[str, List]:
     queryset: Union[Tuple, Coroutine] = await sync_to_async(tuple)(
-        models.User.users.get_users())
+        user_models.User.users.get_users())
 
     return await sync_to_async(lambda: {
         "users": UserSchema.from_django(queryset, many=True)})()
@@ -42,13 +43,16 @@ async def read_users() -> Dict[str, List]:
 
 async def create_user(schema_type: UserSchema) -> UserSchema:
     data: UserSchema = await sync_to_async(lambda: schema_type)()
-    await sync_to_async(lambda: models.User.from_api(data).save())()
+    await sync_to_async(lambda: user_models.User.from_api(data).save())()
     return data
 
 
 @sync_to_async
 def get_user_by_token(token: str):
-    return models.User.users.get_users().filter(id=token)
+    user = user_models.User.users.get_users().filter(email=token)
+    if user.exists():
+        return user.get()
+    return None
 
 
 async def get_current_user(
@@ -56,9 +60,8 @@ async def get_current_user(
 
 ):
     user = await get_user_by_token(token)
-
-    if user.exists():
-        return await user.aget()
+    if user is not None:
+        return user
 
     raise HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
@@ -70,14 +73,9 @@ async def get_current_user(
 async def get_current_active_user(
         current_user: Annotated[UserSchema, Depends(get_current_user)]
 ):
-    if not current_user.is_active:
-        raise HTTPException(status_code=400, detail="Inactive User")
-    return current_user
-
-    # data: UserSchema = await sync_to_async(lambda: current_user)()
-    # if not sync_to_async(models.User.from_api(data).aget().is_active):
-    #     raise HTTPException(status_code=400, detail="Inactive User")
-    # return data
+    if current_user.is_active:
+        return current_user
+    raise HTTPException(status_code=400, detail="Inactive User")
 
 
 @sync_to_async
@@ -92,8 +90,15 @@ async def login(form: Annotated[OAuth2PasswordRequestForm, Depends()]):
     return {"access_token": user.email, "token_type": "bearer"}
 
 
+@sync_to_async
+def to_schema(model: models, schema: ModelSchema, many=False):
+    if not many:
+        return schema.from_orm(model)
+    return schema.from_django(model, many=True)
+
+
 async def read_users_me(current_user: Annotated[UserSchema, Depends(get_current_active_user)]):
-    return current_user
+    return await to_schema(current_user, UserSchema)
 
 
 async def delete_user_me():
