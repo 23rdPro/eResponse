382b5990ded3e8f817c23bc612882c7a490baece
23rdPro <mailolumide@gmail.com>
2023-07-18 20:25:14 +0100

    implemented model delete endpoint for user

--- eResponse/user/FastAPI/views.py
+++ eResponse/user/FastAPI/views.py
@@ -35,91 +35,27 @@ from fastapi.responses import RedirectResponse
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm, OAuth2PasswordRequestFormStrict
 # from eResponse.urls import router
 from eResponse.user import models as user_models
-from eResponse.auth_token import models as auth_token_models
+
 from eResponse.auth_token.FastAPI import schema as auth_token_schema
 
-from eResponse.user.FastAPI.schemas import UserSchema, GroupSchema
+from eResponse.user.FastAPI.schemas import UserSchema, GroupSchema, UserRegistrationSchema
 from eResponse import oauth2_scheme, pwd_context, ALGORITHM, API_SECRET_KEY, ACCESS_TOKEN_EXPIRE_MINUTES
 
-from .utils import create_access_token
+from .utils import (
+    create_access_token, create_user_sync, get_user_from_token,
+    authenticate_user, login_token_async, to_schema, get_all_users,
+    filter_user_by_id, jwt_decode
+)
 
 URL = ""
 
 
-@sync_to_async
-def to_schema(django_obj: Any[models, QuerySet], schema: ModelSchema):
-    if hasattr(django_obj, "count"):  # queryset.count
-        return schema.from_django(django_obj, many=True)
-    return schema.from_orm(django_obj)
-
-
-@sync_to_async
-def from_schema(model: models, schema: ModelSchema) -> None:
-    """
-    create model instance from response schema
-    :param model:
-    :param schema:
-    :return:
-    """
-    instance = model.from_api(schema)
-    try:
-        instance.save()
-    except Exception as Error:
-        raise Error
-    return
-
-
-@sync_to_async
-def authenticate_user(email: str, password: str):
-    return authenticate(email=email, password=password)
-
-
-@sync_to_async
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-@sync_to_async
-def get_user_from_token(token: str):
-    user = user_models.User.users.get_users().filter(email=token)
-    if user.exists():
-        return user.get()
-    return None
-
-
-@sync_to_async
-def get_all_users():
-    return user_models.User.users.get_users()
-
-
-@sync_to_async
-def filter_user_by_id(user_id: str):
-    user = user_models.User.objects.filter(id=user_id)
-    if user.exists():
-        return user.get()
-    return None
-
-
-@sync_to_async
-def login_token_async(user, access_token):
-    auth_token = auth_token_models.Token.objects.select_related("user").filter(user=user.id)
-    if auth_token.exists():
-        auth_token.update(access_token=access_token)
-        return auth_token.get()
-    return auth_token_models.Token.objects.create(access_token=access_token, user=user)
-
-
-@sync_to_async
-def get_user(**kwargs):
-    user = user_models.User.objects.filter(**kwargs)
-    if user.exists():
-        return user.get()
-    return
-
-
-async def create_user(user: UserSchema):
-    await from_schema(user_models.User, user)
-    return user
+async def create_user(*, new_user: UserRegistrationSchema):
+    user = await create_user_sync(**new_user.dict())
+    if user is not None:
+        return new_user
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                        detail="Incorrect email or password")
 
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
@@ -129,15 +65,13 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = await sync_to_async(lambda: jwt.decode(token, API_SECRET_KEY, algorithms=[ALGORITHM]))()
+        payload = await jwt_decode(token)
         email: str = payload.get("sub")
-        if email is None:
+        user = await get_user_from_token(email)
+        if email is None or user is None:
             raise http_exception
-    except JWTError:
-        raise http_exception
 
-    user = await get_user_from_token(email)
-    if user is None:
+    except JWTError:
         raise http_exception
     return user
 
@@ -147,10 +81,8 @@ async def get_current_active_user(
 ):
     if current_user.is_active:
         return current_user
-    raise HTTPException(status_code=400, detail="Inactive User")
-
-
-CurrentActiveUser = Annotated[UserSchema, Depends(get_current_active_user)]
+    raise HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST, detail="Inactive User")
 
 
 async def login(form: Annotated[OAuth2PasswordRequestForm, Depends()]):
@@ -160,6 +92,7 @@ async def login(form: Annotated[OAuth2PasswordRequestForm, Depends()]):
         headers={"WWW-Authenticate": "Bearer"},
     )
     user = await authenticate_user(form.username, form.password)
+    print("Here>>>>>>>>>>>>>>", user)
     if user is None:
         raise http_exception
 
@@ -191,5 +124,11 @@ async def update_user(user: UserSchema, user_id: str):
     return user
 
 
-async def delete_user(*, user: CurrentActiveUser, ):
-    pass
+async def delete_user(*, curr_user: Annotated[UserSchema, Depends(get_current_user)], user_id: str):
+    user = await filter_user_by_id(user_id)
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="user not found")
+    assert hasattr(user, 'delete')
+    await sync_to_async(lambda: user.delete())()
+    return
+
