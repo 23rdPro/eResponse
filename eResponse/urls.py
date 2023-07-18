6edea71f835bfe5fecb28b998341623169a62d59
23rdPro <mailolumide@gmail.com>
2023-07-18 13:23:17 +0100

    refactored path for endpoints

--- eResponse/urls.py
+++ eResponse/urls.py
@@ -9,24 +9,29 @@ from eResponse.user import UserType
 from eResponse.user.FastAPI import views, schemas
 from eResponse.user import models
 
-router = APIRouter()
 
 users = ["users"]
+router = APIRouter(tags=users)
+
+router.post("/token", status_code=status.HTTP_200_OK,
+            summary="jwt login")(views.login)
 
 router.get('/users', dependencies=[Depends(oauth2_scheme)],
-           tags=["users", ], summary="Retrieve all users",
-           status_code=200)(views.read_users)
+           summary="retrieve all users",
+           status_code=status.HTTP_200_OK)(views.read_users)
 
-router.post('/users', tags=["users", ], status_code=200,
-            summary="Post to user")(views.create_user)
-
-router.post("/token", status_code=200, tags=["users"])(views.login)
-
-router.get("/users/me", status_code=200, tags=["users", ]
-           )(views.read_users_me)
-
-router.get("/users/{user_id}", status_code=status.HTTP_200_OK, tags=users
-           )(views.get_user)
+# router.post('/users', status_code=status.HTTP_201_CREATED,
+#             summary="create user")(views.create_user)
+#
+#
+# router.get("/users/me", status_code=200, tags=["users", ]
+#            )(views.read_users_me)
+#
+# router.get("/users/{user_id}", status_code=status.HTTP_200_OK, tags=users
+#            )(views.get_user)
+#
+# router.put("/users/{user_id}", status_code=status.HTTP_200_OK,
+#            tags=users)(views.update_user)
 
 
 urlpatterns = []
