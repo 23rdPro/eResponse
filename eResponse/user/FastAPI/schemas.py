204a03b3831e00db474a988455c4369acb1a5938
23rdPro <mailolumide@gmail.com>
2023-07-18 13:24:12 +0100

    included nested schema in user schema

--- eResponse/user/FastAPI/schemas.py
+++ eResponse/user/FastAPI/schemas.py
@@ -1,3 +1,5 @@
+import typing
+
 from django.contrib.auth.models import Group
 from djantic import ModelSchema
 from eResponse.user import models
@@ -16,8 +18,14 @@ class CertificationSchema(ModelSchema):
 
 
 class UserSchema(ModelSchema):
+    groups: typing.List[GroupSchema]
+    certifications: typing.List[CertificationSchema]
+    # avatar: typing.Optional[str] = None todo
 
     class Config:
         model = models.User
-        include = ['email', "password", ]
+        include = ['email', "password", "name", "is_active",
+                   "is_superuser", "is_staff", "groups",
+                   "is_available", "title", "mobile",
+                   "certifications", ]
 
