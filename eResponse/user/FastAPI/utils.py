5bd50ee1ee891b639b863b25f044446d8cff1499
23rdPro <mailolumide@gmail.com>
2023-07-18 20:24:42 +0100

    separated utilities for coherent api

--- eResponse/user/FastAPI/utils.py
+++ eResponse/user/FastAPI/utils.py
@@ -2,8 +2,15 @@ from __future__ import annotations
 
 from datetime import timedelta, datetime
 from asgiref.sync import sync_to_async
+from typing import Any
 from jose import jwt, JWTError
+from django.db import models
+from django.db.models.query import QuerySet
+from django.contrib.auth import authenticate
+from djantic.main import ModelSchema
 from eResponse import oauth2_scheme, pwd_context, ALGORITHM, API_SECRET_KEY, ACCESS_TOKEN_EXPIRE_MINUTES
+from eResponse.user import models as user_models
+from eResponse.auth_token import models as auth_token_models
 
 
 @sync_to_async
@@ -16,3 +23,86 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, API_SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+@sync_to_async
+def jwt_decode(token):
+    return jwt.decode(token, API_SECRET_KEY, algorithms=[ALGORITHM])
+
+
+@sync_to_async
+def to_schema(django_obj: Any[models, QuerySet], schema: ModelSchema):
+    if hasattr(django_obj, "count"):  # queryset.count
+        return schema.from_django(django_obj, many=True)
+    return schema.from_orm(django_obj)
+
+
+@sync_to_async
+def from_schema(model: models, schema: ModelSchema) -> None:
+    """
+    create model instance from response schema
+    :param model:
+    :param schema:
+    :return:
+    """
+    instance = model.from_api(schema)
+    try:
+        instance.save()
+    except Exception as Error:
+        raise Error
+    return
+
+
+@sync_to_async
+def create_user_sync(**kwargs):
+    user = user_models.User.objects.create_user(**kwargs)
+    return user
+
+
+@sync_to_async
+def authenticate_user(email: str, password: str):
+    return authenticate(email=email, password=password)
+
+
+@sync_to_async
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+@sync_to_async
+def get_user_from_token(token: str):
+    user = user_models.User.users.get_users().filter(email=token)
+    assert user.count() == 1
+    if user.exists():
+        return user.get()
+    return None
+
+
+@sync_to_async
+def get_all_users():
+    return user_models.User.users.get_users()
+
+
+@sync_to_async
+def filter_user_by_id(user_id: str):
+    user = user_models.User.objects.filter(id=user_id)
+    if user.exists():
+        return user.get()
+    return None
+
+
+@sync_to_async
+def login_token_async(user, access_token):
+    auth_token = auth_token_models.Token.objects.select_related("user").filter(user=user.id)
+    if auth_token.exists():
+        auth_token.update(access_token=access_token)
+        return auth_token.get()
+    return auth_token_models.Token.objects.create(access_token=access_token, user=user)
+
+
+@sync_to_async
+def get_user(**kwargs):
+    user = user_models.User.objects.filter(**kwargs)
+    if user.exists():
+        return user.get()
+    return
