b59823929de6876589f35229683f421424fad551
23rdPro <mailolumide@gmail.com>
2023-07-21 17:10:33 +0100

    implemented sync operations for response endpoints

--- eResponse/response/FastAPI/views.py
+++ eResponse/response/FastAPI/views.py
@@ -1,5 +1,22 @@
-async def start_response():
-    pass
+from fastapi import Depends
+from eResponse.user.FastAPI.views import get_current_active_user
+from .schemas import EmergencySchema
+from .utils import start_emergency_sync
+from eResponse.response import models
+
+from asgiref.sync import sync_to_async
+
+CurrentUser = Depends(get_current_active_user)
+
+
+async def start_emergency_response(*, manager: CurrentUser, emergency: EmergencySchema):
+    response = await start_emergency_sync(**emergency.dict())
+    return emergency
+
+
+async def get_emergency_response():
+    responses = await sync_to_async(lambda: models.Emergency.objects.all())()
+    return await sync_to_async(lambda: EmergencySchema.from_django(responses, many=True))()
 
 
 async def update_response():
