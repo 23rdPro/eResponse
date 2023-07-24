6739d4fdd72b72119755c4d31d0357143a798568
23rdPro <mailolumide@gmail.com>
2023-07-24 08:14:38 +0100

    added supporting view for response endpoint

--- eResponse/response/FastAPI/views.py
+++ eResponse/response/FastAPI/views.py
@@ -3,15 +3,19 @@ from eResponse.user.FastAPI.views import get_current_active_user
 from .schemas import EmergencySchema
 from .utils import start_emergency_sync
 from eResponse.response import models
+from eResponse import oauth2_scheme
 
 from asgiref.sync import sync_to_async
 
-CurrentUser = Depends(get_current_active_user)
+CurrentUser = Depends(oauth2_scheme)
 
 
-async def start_emergency_response(*, manager: CurrentUser, emergency: EmergencySchema):
-    response = await start_emergency_sync(**emergency.dict())
-    return emergency
+async def start_emergency_response(*, emergency: EmergencySchema):
+    pass
+
+
+async def create_brief():
+    pass
 
 
 async def get_emergency_response():
