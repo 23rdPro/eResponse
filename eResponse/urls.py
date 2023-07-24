4a859a31017cda7322bf59fb6ddb27db1ab023ad
23rdPro <mailolumide@gmail.com>
2023-07-24 08:16:42 +0100

    published response endpoint object creation

--- eResponse/urls.py
+++ eResponse/urls.py
@@ -12,31 +12,35 @@ from eResponse.user import models
 from eResponse.response.FastAPI.views import start_emergency_response, get_emergency_response
 
 
-tags = ["users",]
-router = APIRouter(tags=tags)
+router = APIRouter()
+
+users = ["users"]
+responses = ["responses"]
 
 router.post("/token", status_code=status.HTTP_200_OK,
-            summary="jwt login")(views.login)
+            summary="jwt login", tags=users)(views.login)
 
 router.get('/users', dependencies=[Depends(oauth2_scheme)],
-           summary="retrieve all users",
+           summary="retrieve all users", tags=users,
            status_code=status.HTTP_200_OK)(views.read_users)
 
 router.post('/users', status_code=status.HTTP_201_CREATED,
-            summary="create user")(views.create_user)
+            summary="create user", tags=users)(views.create_user)
 
-router.get("/users/me", status_code=200, tags=["users", ]
+router.get("/users/me", status_code=200, tags=users,
            )(views.read_users_me)
 
-router.get("/users/{user_id}", status_code=status.HTTP_200_OK)(views.get_user)
+router.get("/users/{user_id}", status_code=status.HTTP_200_OK, tags=users)(views.get_user)
 
-router.patch("/users/{user_id}", status_code=status.HTTP_200_OK)(views.update_user)
+router.patch("/users/{user_id}", status_code=status.HTTP_200_OK, tags=users)(views.update_user)
 
 router.delete("/users/{user_id}", summary="delete current user",
-              status_code=status.HTTP_204_NO_CONTENT,
+              status_code=status.HTTP_204_NO_CONTENT, tags=users
               )(views.delete_user)
 
-router.get("/response", status_code=status.HTTP_200_OK)(get_emergency_response)
+# router.get("/response", status_code=status.HTTP_200_OK)(get_emergency_response)
+
+router.post("/response", status_code=status.HTTP_200_OK, tags=responses)(start_emergency_response)
 
 
 urlpatterns = []
