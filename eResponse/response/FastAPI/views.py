b24adea51d30a6feb4ce3dfbb8353db6b9a98ddd
23rdPro <mailolumide@gmail.com>
2023-07-26 11:42:30 +0100

    refactored start emergency endpoint

--- eResponse/response/FastAPI/views.py
+++ eResponse/response/FastAPI/views.py
@@ -1,11 +1,15 @@
 from typing import Annotated, Optional, List
-from fastapi import Depends, File, UploadFile
-from .schemas import EmergencySchema, BriefSchema
+from fastapi import Depends, File, UploadFile, status
+from starlette.responses import PlainTextResponse, RedirectResponse
+from eResponse.response import RESPONSE_PREFIX
+from .schemas import EmergencySchema, BriefSchema, FileSchema
 from eResponse.user.FastAPI.schemas import GroupSchema
-from .utils import start_emergency_sync
+from .utils import start_emergency_sync, start_emergency_sync, is_user_manager
 from eResponse.response import models
-from eResponse import oauth2_scheme
+from eResponse import oauth2_scheme, PREFIX
 from eResponse.user.FastAPI.views import get_current_user
+from eResponse.user.models import User
+# from eResponse.response.models import Emergency, Brief, File
 
 from asgiref.sync import sync_to_async
 
@@ -23,46 +27,44 @@ router.post("/ers-files/upload/file", )  # upload file
 
 async def start_emergency_response(
         *, manager: Annotated[str, CurrentActiveUser],
-        files: List[UploadFile],
-        brief: BriefSchema,
         emergency: EmergencySchema,
         emergency_type: GroupSchema
 ):
-    """
-    "however a manager must instantiate this model hence"... from model
-    await create brief a must
-    then update emergency with stuff
-    upload pictures, videos
 
-    permission if user in get_managers
+    emergency_type_to_dict = emergency_type.dict()  # Group
+    emergency_to_dict = emergency.dict()
+    emergency_data = {"emergency_type": emergency_type_to_dict.get("name"),
+                      "severity": emergency_to_dict.get("severity")}
+    emergency_instance = Emergency.objects.create(**emergency_data)
 
-    """
-    # print(manager.id, type(manager))
-    # print(emergency, type(emergency))
-    # await start_emergency_sync(emergency, manager)
-    # brief = await create_brief(manager, )
-    # picture = await upload_files
+    # however a manager must instantiate this model hence"... from model
+    emergency_instance.respondents.add(manager)
+    # respondents cannot be blank, but because we need to wait for files
+    # before creating emergency leading to response redirect after successive segments:
+    # we made briefs accept blank in model to allow us ultimately call save() on emergency
 
-    files = [models.File.objects.create(file=file.file) for file in files]
+    emergency_instance.save()
+
+    return RedirectResponse(url=f"{PREFIX}/{RESPONSE_PREFIX}/add-brief/{str(emergency_instance.id)}/{str(manager.id)}",
+                            status_code=status.HTTP_307_TEMPORARY_REDIRECT)
+
+
+async def create_brief(emergency_id: str, brief: BriefSchema, user_id: str):
+    emergency = Emergency.objects.get(emergency_id)
+    user = User.objects.get(user_id)
 
     brief_to_dict = brief.dict()
-    brief_data = {"reporter": manager, "title": brief_to_dict.get("title"), "text": brief_to_dict.get("text")}
-    brief_instance = models.Brief.objects.create(**brief_data)
-    brief_instance.files.add(*files)
+    brief_data = {"reporter": user, "title": brief_to_dict.get("title"), "text": brief_to_dict.get("text")}
+    brief_instance = Brief.objects.create(**brief_data)
 
-    emergency_to_dict = emergency.dict()
-    emergency_type_to_dict = emergency_type.dict()
+    emergency.briefs.add(brief_instance)
+    emergency.save()
 
-    emergency_data = {"emergency_type": emergency_type_to_dict.get("name"), "severity": emergency_to_dict.get("severity")}
-    emergency_instance = models.Emergency.objects.create(**emergency_data)
-    emergency_instance.respondents.add(manager)
-    emergency_instance.briefs.add(brief_instance)
-
-    "async"
+    return RedirectResponse(url=f"{PREFIX}/{RESPONSE_PREFIX}/add-files/{str(emergency.id)}",
+                            status_code=status.HTTP_307_TEMPORARY_REDIRECT)
 
 
-async def create_brief():
-    pass
+async def upload_files(files: List[UploadFile], emergency_id: str)
 
 
 async def get_emergency_response():
