b81734d830b86bc27d81362ddb4c9d2f773d4f54
23rdPro <mailolumide@gmail.com>
2023-07-26 13:21:45 +0100

    refactored start emergency endpoints

--- eResponse/response/FastAPI/views.py
+++ eResponse/response/FastAPI/views.py
@@ -4,7 +4,15 @@ from starlette.responses import PlainTextResponse, RedirectResponse
 from eResponse.response import RESPONSE_PREFIX
 from .schemas import EmergencySchema, BriefSchema, FileSchema
 from eResponse.user.FastAPI.schemas import GroupSchema
-from .utils import start_emergency_sync, start_emergency_sync, is_user_manager
+from eResponse.api_helpers import to_schema
+from .utils import (
+    start_emergency_sync,
+    start_emergency_sync,
+    is_user_manager,
+    start_emergency_response_sync,
+    create_brief_sync,
+    upload_files_sync,
+)
 from eResponse.response import models
 from eResponse import oauth2_scheme, PREFIX
 from eResponse.user.FastAPI.views import get_current_user
@@ -18,53 +26,34 @@ from eResponse.response.models import Emergency, Brief
 CurrentUser = Depends(oauth2_scheme)
 CurrentActiveUser = Depends(get_current_user)
 
-"""
-router.post("/ers-files/{user_id}/{mime_type}", )  # create_file
-router.post("/ers-files/upload/file", )  # upload file
-
-"""
-
 
 async def start_emergency_response(
         *, manager: Annotated[str, CurrentActiveUser],
         emergency: EmergencySchema,
         emergency_type: GroupSchema
 ):
-
-    emergency_type_to_dict = emergency_type.dict()  # Group
-    emergency_to_dict = emergency.dict()
-    emergency_data = {"emergency_type": emergency_type_to_dict.get("name"),
-                      "severity": emergency_to_dict.get("severity")}
-    emergency_instance = Emergency.objects.create(**emergency_data)
-
-    # however a manager must instantiate this model hence"... from model
-    emergency_instance.respondents.add(manager)
-    # respondents cannot be blank, but because we need to wait for files
-    # before creating emergency leading to response redirect after successive segments:
-    # we made briefs accept blank in model to allow us ultimately call save() on emergency
-
-    emergency_instance.save()
+    _data = {"manager": manager, "emergency": emergency, "emergency_type": emergency_type}
+    emergency_instance = await start_emergency_response_sync(**_data)
 
     return RedirectResponse(url=f"{PREFIX}/{RESPONSE_PREFIX}/add-brief/{str(emergency_instance.id)}/{str(manager.id)}",
                             status_code=status.HTTP_307_TEMPORARY_REDIRECT)
 
 
 async def create_brief(emergency_id: str, brief: BriefSchema, user_id: str):
-    emergency = Emergency.objects.get(emergency_id)
-    user = User.objects.get(user_id)
+    _data = {"emergency_id": emergency_id, "brief": brief, "user_id": user_id}
+    brief_instance: Emergency = await create_brief_sync(**_data)
 
-    brief_to_dict = brief.dict()
-    brief_data = {"reporter": user, "title": brief_to_dict.get("title"), "text": brief_to_dict.get("text")}
-    brief_instance = Brief.objects.create(**brief_data)
-
-    emergency.briefs.add(brief_instance)
-    emergency.save()
-
-    return RedirectResponse(url=f"{PREFIX}/{RESPONSE_PREFIX}/add-files/{str(emergency.id)}",
+    return RedirectResponse(url=f"{PREFIX}/{RESPONSE_PREFIX}/add-files/{str(brief_instance.id)}",
                             status_code=status.HTTP_307_TEMPORARY_REDIRECT)
 
 
-async def upload_files(files: List[UploadFile], emergency_id: str)
+async def upload_files(files: List[UploadFile], emergency_id: str):
+    _data = {"files": files, "emergency_id": emergency_id}
+    file_instance: Emergency = await upload_files_sync(**_data)
+
+    return await to_schema(file_instance, EmergencySchema)
+
+
 
 
 async def get_emergency_response():
