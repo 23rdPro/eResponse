f17c718806b585ba7d961d0970c011b770784f9d
23rdPro <mailolumide@gmail.com>
2023-07-30 10:51:33 +0100

    refactored init response endpoint

--- eResponse/response/FastAPI/views.py
+++ eResponse/response/FastAPI/views.py
@@ -4,7 +4,7 @@ import aiofiles
 from fastapi import Depends, File, UploadFile, status
 from django.core.files import File as DjangoFile
 from django.contrib.auth.models import Group
-from django.db import transaction
+
 from starlette.responses import PlainTextResponse, RedirectResponse
 from eResponse.response import RESPONSE_PREFIX
 from .schemas import (
@@ -31,6 +31,9 @@ from .utils import (
     schema_to_dict,
     brief_data_from_e_dict_sync,
     create_file_sync,
+    get_model_object,
+    create_response_sync,
+    transaction_atomic_file
 )
 from eResponse.response import models
 from eResponse import oauth2_scheme, PREFIX
@@ -46,117 +49,56 @@ CurrentUser = Depends(oauth2_scheme)
 CurrentActiveUser = Depends(get_current_user)
 
 
-@sync_to_async
-@transaction.atomic
-def transaction_atomic_files():
-    d_file = models.File()
-    d_file.save()
-    return d_file
+async def init_response(
+        manager: Annotated[str, CurrentActiveUser],
+        action: CreateEmergencyResponseSchema = Depends(),
+        brief: BriefSchema = Depends(),
+        files: List[UploadFile] = File(...),
+):
 
+    b_data_dict = brief.dict()
+    b_data = {"title": b_data_dict["title"], "text": b_data_dict["text"], "reporter": manager}
+    e_brief = await create_brief_sync(**b_data)
 
-async def file_upload(user: Annotated[str, CurrentActiveUser], files: List[UploadFile] = File(...)):
-    filez = []
     for file in files:
-
         async with aiofiles.open(f"eResponse/media/{file.filename}", "wb") as media:
             content = await file.read()
             await media.write(content)
+        b_file = await transaction_atomic_file()
+        await sync_to_async(lambda: e_brief.files.add(b_file))()
 
-            d_file = await transaction_atomic_files()
-            filez.append(d_file.id)
+    action_dict = action.dict()
+    action_data = {
+        "emergency_type": await get_model_object(
+            **{"name": action_dict["emergency_type"], "model": Group}
+        ),
+        "severity": action_dict["severity"],
 
-    # filez = await models.File.objects.afilter(id__in=(f.id for f in filez)).all()
+    }
+    response = await create_response_sync(**action_data)
+    await sync_to_async(lambda: response.respondents.add(manager))()
+    await sync_to_async(lambda: response.briefs.add(e_brief))()
 
-    # filez = await to_schema(filez, FileSchema)
-    # user = await to_schema(user, UserSchema)
-
-    # await sync_to_async(lambda: print(filez, "<<<<<<<<>>>>>>>>"))()
-
-    return RedirectResponse(url=f"{PREFIX}/emergencies/{filez}/{user.id}")
+    return await to_schema(response, EmergencySchema)
 
 
-async def create_emergency_response(
-        emerg: CreateEmergencyResponseSchema,
-        files: List[str],
-        user: str
+def update_response(
+        user: Annotated[str, CurrentActiveUser],
 
 ):
-    await sync_to_async(lambda: print(emerg.dict()))()
-    return emerg
-
-
-    # return files
-    # async def _upload_files(files: List[UploadFile]):
-    #     return await create_files_sync(files)
-    #
-    # filez = _upload_files(files=)
-
-
-    # pass
-    # await sync_to_async(lambda: print(emg))()
-    # return emg
-    # filez: list = await create_files_sync(files)
-    # print(filez)
-    #
-    # emg_dict = await schema_to_dict(emg)
-    #
-    # brief_data = await brief_data_from_e_dict_sync(emg_dict, manager)
-    # brief = await create_brief_sync(filez, brief_data)
-    #
-    # group_name = await sync_to_async(lambda: emg_dict.get("emergency_type").get("name"))()
-    # emergency_group = await create_emergency_group_sync(group_name)
-    #
-    # severity = await sync_to_async(lambda: emg_dict.get("severity"))()
-    # emergency_data = {"emergency_type": emergency_group, "severity": severity}
-    #
-    # emergency = await create_emergency_response_sync(emergency_data, brief, manager)
-    #
-    # return await to_schema(emergency, EmergencySchema)
-
-
-async def upload_files(files: List[UploadFile], emergency_id: str):
-    emergency = await create_files_sync(files, emergency_id)
-    return await to_schema(emergency, EmergencySchema)
-
-
-async def start_emergency_response(
-        *, manager: Annotated[str, CurrentActiveUser],
-        emergency: Emergency,
-        # emergency_type: GroupSchema
-):
-    _data = {"manager": manager, "emergency": emergency}  # "emergency_type": emergency_type
-    emergency_instance = await start_emergency_response_sync(**_data)
-
-    return RedirectResponse(url=f"{PREFIX}/{RESPONSE_PREFIX}/add-brief/{emergency_instance.id}/{manager.id}",
-                            )
-
-
-async def create_brief(emergency_id: str, user_id: str, brief: Annotated[str, BriefSchema]):
-    await sync_to_async(lambda: print(emergency_id))()
-    _data = {"emergency_id": emergency_id, "brief": brief, "user_id": user_id}
-    brief_instance: Emergency = await create_brief_sync(**_data)
-
-    return RedirectResponse(url=f"{PREFIX}/{RESPONSE_PREFIX}/add-files/{str(brief_instance.id)}",
-                            status_code=status.HTTP_307_TEMPORARY_REDIRECT)
-
-
-# async def upload_files(files: List[UploadFile], emergency_id: str):
-#     _data = {"files": files, "emergency_id": emergency_id}
-#     file_instance: Emergency = await upload_files_sync(**_data)
-#
-#     return await to_schema(file_instance, EmergencySchema)
-
-
-async def get_emergency_response():
-    responses = await sync_to_async(lambda: models.Emergency.objects.all())()
-    return await sync_to_async(lambda: EmergencySchema.from_django(responses, many=True))()
-
-
-async def update_response():
     pass
 
 
-async def delete_response():
+def delete_response():
     pass
+
+
+def get_responses():
+    pass
+
+
+def get_response():
+    pass
+
 
 
