837ced8e833c5c6c836a9827b58eae810c10bcee
23rdPro <mailolumide@gmail.com>
2023-07-31 19:16:15 +0100

    added model methods for response endpoint

--- eResponse/api_helpers.py
+++ eResponse/api_helpers.py
@@ -7,6 +7,12 @@ from django.db.models import QuerySet
 
 @sync_to_async
 def to_schema(django_obj: Union[type(models), type(QuerySet)], schema: ModelSchema):
-    if hasattr(django_obj, "count"):  # queryset.count
-        return schema.from_django(django_obj, many=True)
+    if hasattr(django_obj, "count"):
+        if django_obj.count() > 1:
+            return schema.from_django(django_obj, many=True)
+        return schema.from_orm(django_obj.get())
     return schema.from_orm(django_obj)
+
+    # if hasattr(django_obj, "count"):  # queryset.count
+    #     return schema.from_django(django_obj, many=True)
+    # return schema.from_orm(django_obj)
